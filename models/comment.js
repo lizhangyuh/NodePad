962c2533ff28d406724aa0f429861adce48310aa
Jerry Lee <lizhangyuh@gmail.com>
2014-10-17 01:10:28 +0800

    评论模块

---
+++ models/comment.js
@@ -0,0 +1,34 @@
+var mongoose = require('./db');
+
+function Comment(obj) {
+    this.pinyin = obj.pinyin;
+    this.comment = obj.comment
+}
+
+var postsSchema = mongoose.Schema({
+    pinyin: String,
+    comments:[{name:String,content:String,time:String}]
+},{collection:'posts'});
+
+var postsModel = mongoose.model('Comment', postsSchema);
+
+module.exports = Comment;
+
+//存储一条留言信息
+Comment.prototype.save = function(callback) {
+    var pinyin = this.pinyin;
+    var comment = this.comment;
+    postsModel.findOne({pinyin:pinyin},function(err, post){
+        if(err){
+            return callback(err);
+        }
+        post.comments = post.comments.unshift(comment);
+        postsModel.findOneAndUpdate({pinyin:pinyin},{comments:post.comments},null,function(err){
+            if(err){
+                console.log(err);
+                return callback(err);
+            }
+                callback(null);
+        });
+    });
+}
